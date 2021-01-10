059c8f3bb859b15076fed1b1d4ffae3662a4b31e
codemantra10 <73803525+codemantra10@users.noreply.github.com>
2021-01-09 19:30:54 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function calculate(){
+    var number1= document.getElementById("number1").value;
+    var number2= document.getElementById("number2").value;
+    var number3= document.getElementById("number3").value;
+    var add= number1+number2;
+    var subtract= number1-number2;
+    var divide= number1/number2;
+    var multiply= number1*number2;
+    var square=Math.sqrt(number3) 
+    console.log(add);
+    console.log(subtract);
+    console.log(divide);
+    console.log(multiply);
+    console.log(square);
+    console.log("nextnumber");
+    
+}
